e666807369b753588b935890ae5e04486ef81773
Abdelrhman Hani <abdohani362@gmail.com>
2022-12-16 04:51:17 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,76 @@
+
+const sub = document.getElementById('submit')
+const rating = document.getElementById('rating')
+const thanks = document.getElementById('thanks')
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+const btn3 = document.getElementById('btn3')
+const btn4 = document.getElementById('btn4')
+const btn5 = document.getElementById('btn5')
+const par = document.getElementById('par')
+const score = document.getElementsByClassName('btn')
+let sco 
+
+btn1.addEventListener('click',()=>{
+  sco = 1
+  btn1.style.color = "#fff" 
+  btn1.style.backgroundColor = "#fb7413"
+  btn2.style.backgroundColor = "#384354"
+  btn3.style.backgroundColor = "#384354"
+  btn4.style.backgroundColor = "#384354"
+  btn5.style.backgroundColor = "#384354"
+  par.innerText= `You selected ${sco} out of 5`
+  
+})
+
+btn2.addEventListener('click',()=>{
+  sco = 2
+  btn2.style.color = "#fff" 
+  btn2.style.backgroundColor = "#fb7413"
+  btn1.style.backgroundColor = "#384354"
+  btn3.style.backgroundColor = "#384354"
+  btn4.style.backgroundColor = "#384354"
+  btn5.style.backgroundColor = "#384354"
+  par.innerText= `You selected ${sco} out of 5`
+})
+
+btn3.addEventListener('click',()=>{
+  sco = 3
+  btn3.style.color = "#fff" 
+  btn3.style.backgroundColor = "#fb7413"
+  btn1.style.backgroundColor = "#384354"
+  btn2.style.backgroundColor = "#384354"
+  btn4.style.backgroundColor = "#384354"
+  btn5.style.backgroundColor = "#384354"
+  par.innerText= `You selected ${sco} out of 5`
+
+})
+
+btn4.addEventListener('click',()=>{
+  sco = 4
+  btn4.style.color = "#fff" 
+  btn4.style.backgroundColor = "#fb7413"
+  btn1.style.backgroundColor = "#384354"
+  btn2.style.backgroundColor = "#384354"
+  btn3.style.backgroundColor = "#384354"
+  btn5.style.backgroundColor = "#384354"
+  par.innerText= `You selected ${sco} out of 5`
+
+})
+
+btn5.addEventListener('click',()=>{
+  sco = 5
+  btn5.style.color = "#fff" 
+  btn5.style.backgroundColor = "#fb7413"
+  btn1.style.backgroundColor = "#384354"
+  btn2.style.backgroundColor = "#384354"
+  btn3.style.backgroundColor = "#384354"
+  btn4.style.backgroundColor = "#384354"
+  par.innerText= `You selected ${sco} out of 5`
+
+})
+
+sub.onclick = function next() {
+  rating.style.display = "none"
+  thanks.style.display = "block"
+}
